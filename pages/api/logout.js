cd35c8d540c3e662564a494ec8c4e6608ac2c68f
ramadlana <hidayahweb@gmail.com>
2025-10-30 21:42:04 +0700

    ok

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,9 +1,11 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
+
   res.setHeader(
     "Set-Cookie",
     "dashx-token=deleted; Path=/; Max-Age=0; HttpOnly; SameSite=Strict"
   );
-  // return redirect to login page
-  return res.redirect("/login");
+
+  // Respond with success, let client handle redirect
+  return res.status(200).json({ success: true });
 }
